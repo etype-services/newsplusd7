154140303294e9d25f3eb20084295c17dc12f190
Charlie Llewellin <charlie@llewellin.net>
2017-10-24 11:58:02 -0500

    freeman sd

--- sites/all/themes/newsplus/template.php
+++ sites/all/themes/newsplus/template.php
@@ -809,18 +809,16 @@ function newsplus_preprocess_page(&$variables) {
   $three_columns_grid_layout = theme_get_setting('three_columns_grid_layout', 'newsplus');
   if (isset ($variables['page'])) {
 
-    var_dump($variables['page']);
-    exit;
-    if (isset ($variables['page']['sidebar_first'])) $sidebar_first = $variables['page']['sidebar_first'];
-    if (isset ($variables['page']['sidebar_second'])) $sidebar_second = $variables['page']['sidebar_second'];
-    if (isset ($variables['page']['footer_first'])) $footer_first = $variables['page']['footer_first'];
-    if (isset ($variables['page']['footer_second'])) $footer_second = $variables['page']['footer_second'];
-    if (isset ($variables['page']['footer_third'])) $footer_third = $variables['page']['footer_third'];
-    if (isset ($variables['page']['footer_fourth'])) $footer_fourth = $variables['page']['footer_fourth'];
-    if (isset ($variables['page']['pre_header_left'])) $pre_header_left = $variables['page']['pre_header_left'];
-    if (isset ($variables['page']['pre_header_right'])) $pre_header_right = $variables['page']['pre_header_right'];
-    if (isset ($variables['page']['header'])) $header = $variables['page']['header'];
-    if (isset ($variables['page']['header_top_right'])) $header_top_right = $variables['page']['header_top_right'];
+    if (!empty($variables['page']['sidebar_first'])) $sidebar_first = $variables['page']['sidebar_first'];
+    if (!empty($variables['page']['sidebar_second'])) $sidebar_second = $variables['page']['sidebar_second'];
+    if (!empty($variables['page']['footer_first'])) $footer_first = $variables['page']['footer_first'];
+    if (!empty($variables['page']['footer_second'])) $footer_second = $variables['page']['footer_second'];
+    if (!empty($variables['page']['footer_third'])) $footer_third = $variables['page']['footer_third'];
+    if (!empty($variables['page']['footer_fourth'])) $footer_fourth = $variables['page']['footer_fourth'];
+    if (!empty($variables['page']['pre_header_left'])) $pre_header_left = $variables['page']['pre_header_left'];
+    if (!empty($variables['page']['pre_header_right'])) $pre_header_right = $variables['page']['pre_header_right'];
+    if (!empty($variables['page']['header'])) $header = $variables['page']['header'];
+    if (!empty($variables['page']['header_top_right'])) $header_top_right = $variables['page']['header_top_right'];
   }
 
   /**
