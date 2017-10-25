46ca6c34923df302c676f00b219023fb731fd763
Charlie Llewellin <charlie@llewellin.net>
2017-10-25 11:06:14 -0500

    synery template fixes

--- sites/all/modules/etype/js/etype.js
+++ sites/all/modules/etype/js/etype.js
@@ -5,10 +5,9 @@
     // this function is to remove the active class from dropdown menus
     attach: function () {
       var pathname = window.location.pathname; // Returns path only
-      console.log(pathname);
       // only on Home Page
       if (pathname === "/") {
-        //$(".tb-megamenu-nav > li").hasClass("dropdown").removeClass("active");
+        $(".tb-megamenu-nav > li");
       }
     }
   };
