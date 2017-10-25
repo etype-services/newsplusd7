cd71d218bbbde90d76a8c93a4ca4806692e4fc21
Charlie Llewellin <charlie@llewellin.net>
2017-10-25 11:55:07 -0500

    synery template fixes

--- sites/all/modules/etype/js/etype.js
+++ sites/all/modules/etype/js/etype.js
@@ -2,7 +2,10 @@
 
 (function ($) {
   Drupal.behaviors.module_etype = {
-    // this function is to remove the active class from dropdown menus
+    // this function is to remove the active class from the main item of
+    // a dropdown menu in the Synery Template
+    // So that that item can be given a path of / but not be active on the
+    // home page. You cannot have an empty path in the Drupal menu system
     attach: function () {
       var pathname = window.location.pathname; // Returns path only
       // only on Home Page
