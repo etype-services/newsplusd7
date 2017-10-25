7e0b520e17aea61ce2f7a098530d7f281c622119
Charlie Llewellin <charlie@llewellin.net>
2017-10-25 10:14:19 -0500

    synery template fixes

--- sites/all/modules/etype/js/etype.js
+++ sites/all/modules/etype/js/etype.js
@@ -1,8 +1,10 @@
 /* add custom js */
 
-(function($){
+(function ($) {
   Drupal.behaviors.module_etype = {
     attach: function () {
+      var pathname = window.location.pathname; // Returns path only
+      console.log(pathname);
       $(".tb-megamenu-nav > li").each(function (i) {
         console.log(i);
       });
