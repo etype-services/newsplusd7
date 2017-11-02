f207fa17d2f62636303119d5f2383e2293b96517
Charlie Llewellin <charlie@llewellin.net>
2017-11-02 09:09:01 -0500

    newsplus theme changes

--- sites/all/themes/newsplus/field--field-ad-image--article.tpl.php
+++ sites/all/themes/newsplus/field--field-ad-image--article.tpl.php
@@ -1,4 +1,4 @@
-<div class="field_ad_image">
-  <div class="field_ad_image_inner">
+<div class="field-ad-image">
+  <div class="field-ad-image-inner">
     <a href="<?php print $img_url ?>"><img src="<?php print $img_src; ?>" /></a></div>
 </div>
