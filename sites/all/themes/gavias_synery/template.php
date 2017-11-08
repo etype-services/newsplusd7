183fab16ad71a3ec89066780b526db8a26d82dd9
Charlie Llewellin <charlie@llewellin.net>
2017-11-08 15:25:29 -0600

    taxonomy pages

--- sites/all/themes/gavias_synery/template.php
+++ sites/all/themes/gavias_synery/template.php
@@ -114,7 +114,7 @@ function gavias_synery_process_html(&$vars) {
 function gavias_synery_process_node(&$vars) {
   if ($vars['view_mode'] === 'teaser') {
     $count = count($vars['content']['field_image']);
-    dpm($count);
+    // dpm($count);
   }
 }
 
