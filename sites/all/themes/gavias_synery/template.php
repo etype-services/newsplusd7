a946210b7512799742e738f3d79bad97e016cd83
Charlie Llewellin <charlie@llewellin.net>
2017-11-09 16:33:28 -0600

    taxonomy pages

--- sites/all/themes/gavias_synery/template.php
+++ sites/all/themes/gavias_synery/template.php
@@ -114,7 +114,7 @@ function gavias_synery_process_html(&$vars) {
 function gavias_synery_process_node(&$vars) {
   $node = node_load('node', $vars['nid']);
   $wrapper = entity_metadata_wrapper('node', $node);
-  echo $wrapper->field_dateline->value;
+  dpm($wrapper->field_dateline->value);
 }
 
 /**
