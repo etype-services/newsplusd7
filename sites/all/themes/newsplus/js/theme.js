a4ef00001b8ecfe86cd827d10229002dfe6f7012
Charlie Llewellin <charlie@llewellin.net>
2017-11-10 16:49:37 -0600

    font awesome

--- sites/all/themes/newsplus/js/theme.js
+++ sites/all/themes/newsplus/js/theme.js
@@ -5,7 +5,7 @@
   Drupal.behaviors.MenuFixer = {
     attach: function(context, settings){
       $("#superfish-1").find("a.menuparent").each(function(i, obj) {
-        $(this).append('<i class="fa fa-caret-down"></i>');
+        $(this).append('&nbsp;<i class="fa fa-caret-down"></i>');
       });
     }
   };
