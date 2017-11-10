7954d4639fbb3a9888c6c92c28f83e41000390a3
Charlie Llewellin <charlie@llewellin.net>
2017-11-10 16:38:21 -0600

    font awesome

--- sites/all/themes/newsplus/js/theme.js
+++ sites/all/themes/newsplus/js/theme.js
@@ -4,8 +4,7 @@
   'use strict';
   Drupal.behaviors.MenuFixer = {
     attach: function(context, settings){
-      $("#header-top ul.sf-menu li").each(function(i, obj) {
-        alert(obj);
+      $("li").each(function(i, obj) {
         console.log(obj);
       });
     }
