d6be8153e42b4764f74f24dfa998392826247aae
Charlie Llewellin <charlie@llewellin.net>
2017-11-10 16:43:22 -0600

    font awesome

--- sites/all/themes/newsplus/js/theme.js
+++ sites/all/themes/newsplus/js/theme.js
@@ -4,7 +4,7 @@
   'use strict';
   Drupal.behaviors.MenuFixer = {
     attach: function(context, settings){
-      $("#header-top").each(function(i, obj) {
+      $("#superfish-1").find(".menuparent").each(function(i, obj) {
         console.log(obj);
       });
     }
