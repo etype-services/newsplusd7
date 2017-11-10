6e91cec8a0d97c10fb6d3c519ed0dc3deeccc8b7
Charlie Llewellin <charlie@llewellin.net>
2017-11-10 16:24:51 -0600

    font awesome

--- sites/all/themes/newsplus/js/theme.js
+++ sites/all/themes/newsplus/js/theme.js
@@ -5,7 +5,7 @@
   Drupal.behaviors.MenuFixer = {
     attach: function(context, settings){
       $('#header-top ul.sf-menu li a.menuparent').each(function(i, obj) {
-        console.log(i);
+        console.log(obj);
       });
     }
   };
