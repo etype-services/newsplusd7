cd54b177a668207b70bbe754d6a6bf5ca38338fe
Charlie Llewellin <charlie@llewellin.net>
2017-11-10 14:21:25 -0600

    font awesome

--- sites/all/themes/newsplus/html.tpl.php
+++ sites/all/themes/newsplus/html.tpl.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces;?>>
-
 <head profile="<?php print $grddl_profile; ?>">
-  <?php print $head; ?>
-  <title><?php print $head_title; ?></title>
-  <?php print $styles; ?>
-
-  <!-- HTML5 element support for IE6-8 -->
-  <!--[if lt IE 9]>
+<?php print $head; ?>
+<title><?php print $head_title; ?></title>
+<?php print $styles; ?>
+<!-- HTML5 element support for IE6-8 -->
+<!--[if lt IE 9]>
     <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-  <![endif]-->
-  <?php print $scripts; ?>
+ <![endif]-->
+<?php print $scripts; ?>
+<script src="https://use.fontawesome.com/cbba4b6297.js"></script>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <div id="skip-link">
