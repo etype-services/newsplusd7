4ae5bb6d84db02f0edaed60e93bf81e0917a468e
Charlie Llewellin <charlie@llewellin.net>
2017-11-13 15:07:48 -0600

    ga

--- sites/all/themes/gavias_synery/template.php
+++ sites/all/themes/gavias_synery/template.php
@@ -103,9 +103,22 @@ function gavias_synery_preprocess_html(&$vars) {
   // add theme identifier
   $vars['classes_array'][] = 'synergy';
 
+  /* Google Analytics code */
   $etype_google_analytics_property_id = variable_get('etype_google_analytics_property_id');
   if (! empty($etype_google_analytics_property_id)) {
-
+    drupal_add_js('
+(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\');
+ga(\'create\', \'' .$etype_google_analytics_property_id . '\', \'auto\');
+ga(\'send\', \'pageview\');
+    ', [
+        'type' => 'inline',
+        'scope' => 'header',
+        'weight' => 50,
+      ]
+    );
   }
 }
 
