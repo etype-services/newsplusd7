7d5ea53543c1106d59bc6bafa9aee1cee06ee64c
Charlie Llewellin <charlie@llewellin.net>
2017-11-14 10:21:42 -0600

    font awesome quick fixes

--- sites/all/themes/newsplus/js/theme.js
+++ sites/all/themes/newsplus/js/theme.js
@@ -2,17 +2,14 @@
 
 (function($) {
   'use strict';
-  Drupal.behaviors.MenuFixer = {
+  Drupal.behaviors.FontFixer = {
     attach: function(context, settings){
       $("#superfish-1").find("a.menuparent").each(function(i, obj) {
         $(this).append('&nbsp;<i class="fa fa-caret-down" aria-hidden="true"></i>');
       });
-    }
-  };
-  Drupal.behaviors.SearchFixer = {
-    attach: function (context, settings) {
       $(".block-search .form-actions").append('<i class="fa fa-search"' +
           ' aria-hidden="true"></i>');
+      $(".more-link a:after").append('<i class="fa fa-search" aria-hidden="true"></i>');
     }
   };
 })(jQuery);
