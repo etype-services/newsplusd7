6711e1470d7ea5b9968e1f280a17b75d9fd5e004
Charlie Llewellin <charlie@llewellin.net>
2017-11-18 13:33:06 -0500

    carousel

--- sites/all/themes/gavias_synery/js/etype.js
+++ sites/all/themes/gavias_synery/js/etype.js
@@ -16,6 +16,10 @@
           }
         });
       }
+      $(".jcarousel-prev-horizontal").append('<i class="fa fa-chevron-left"' +
+          ' aria-hidden="true"></i>');
+      $(".jcarousel-next-horizontal").append('<i class="fa fa-chevron-right"' +
+          ' aria-hidden="true"></i>');
     }
   };
 })(jQuery);
