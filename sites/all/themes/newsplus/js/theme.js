b297a33441cb8d7405b09ef70a8104c6ef8427e1
Charlie Llewellin <charlie@llewellin.net>
2017-11-27 13:16:31 -0500

    the chronicle

--- sites/all/themes/newsplus/js/theme.js
+++ sites/all/themes/newsplus/js/theme.js
@@ -7,7 +7,8 @@
       $("#superfish-1").find("a.menuparent").each(function(i, obj) {
         $(this).append('&nbsp;<i class="fa fa-caret-down" aria-hidden="true"></i>');
       });
-      $(".block-search .form-actions").append('<i class="fa fa-search"' +
+      $(".block-search .form-actions, .block-search .trigger").append('<i' +
+          ' class="fa fa-search"' +
           ' aria-hidden="true"></i>');
       $(".more-link a").append('<i class="fa fa-chevron-right" aria-hidden="true"></i>');
     }
