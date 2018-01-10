c3bc7292425bf3497b337351cd489a6084d5b2b7
Charlie Llewellin <charlie@llewellin.net>
2018-01-10 08:42:47 -0600

    lampasas css

--- sites/all/themes/newsplus/template.php
+++ sites/all/themes/newsplus/template.php
@@ -1058,7 +1058,7 @@ function newsplus_preprocess_field(&$vars)
             ('sites/all/themes/newsplus/field--field-ad-image--article.tpl.php', $build);
         }
     }
-    $vars['items'][0]['#markup'] = $markup;
+    // $vars['items'][0]['#markup'] = $markup;
 }
 
 /**
