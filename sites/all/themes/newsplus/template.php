ca97ae83e44baa6281fe47bd6d6f4939af44ac9b
Charlie Llewellin <charlie@llewellin.net>
2018-01-10 09:23:46 -0600

    lampasas css

--- sites/all/themes/newsplus/template.php
+++ sites/all/themes/newsplus/template.php
@@ -1043,7 +1043,7 @@ function newsplus_preprocess_field(&$vars)
     $markup = '';
     if($vars['element']['#field_name'] == 'field_ad_image')
     {
-        dpm($vars);
+        // dpm($vars);
         $node = node_load($vars['element']['#object']->nid);
         $ad = field_get_items('node', $node, 'field_ad_image');
         if (count($ad) > 0) {
