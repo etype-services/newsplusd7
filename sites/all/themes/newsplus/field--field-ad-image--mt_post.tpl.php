5d6441cd59232036218622d0293eba545b82388d
Charlie Llewellin <charlie@llewellin.net>
2018-01-11 09:58:51 -0600

    spansor ads

--- sites/all/themes/newsplus/field--field-ad-image--mt_post.tpl.php
+++ sites/all/themes/newsplus/field--field-ad-image--mt_post.tpl.php
@@ -1,5 +1,5 @@
 <div class="field-ad-image-wrapper">
-    <?php foreach ($items as $arr): ?>
+    <?php foreach ($field_ad_image_items as $arr): ?>
         <div class="field-ad-image">
             <div class="field-ad-image-inner">
                 <a href="<?php print $arr['img_url'] ?>" target="_blank"><img src="<?php print $arr['img_src']; ?>" /></a></div>
