85e490369a0b12b1785bcdb8fe34d5ae61ef8d10
Charlie Llewellin <charlie@llewellin.net>
2018-01-30 10:33:36 -0600

    lampasas archive search

--- sites/lampasasdispatchrecord.etypegoogle10.com/site.js
+++ sites/lampasasdispatchrecord.etypegoogle10.com/site.js
@@ -1,7 +1,7 @@
-(function ($) {
+/*(function ($) {
     Drupal.behaviors.lampasas = {
         attach: function () {
             $(".social-bookmarks").prepend('<li><a href="http://newzgroup.com/pubarchive/index.php/main/search/4B4FFDC5891E6B7A97CFAF32F39FBAB72A440326/91154" target="_blank"><i class="fa fa-search"></i></a></li>');
         }
     };
-})(jQuery);
+})(jQuery);*/
