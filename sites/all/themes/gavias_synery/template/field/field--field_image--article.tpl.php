539e47d6666fe819a880ffed2bc629a3891544a7
Charlie Llewellin <charlie@llewellin.net>
2018-01-31 15:00:59 -0600

    lampasas search

--- sites/all/themes/gavias_synery/template/field/field--field_image--article.tpl.php
+++ sites/all/themes/gavias_synery/template/field/field--field_image--article.tpl.php
@@ -46,7 +46,7 @@
  * @ingroup themeable
  */
 
-var_dump($items);
+var_dump($items[0]['gallery']['#gallery_images']);
 if (count($items) == 1) { ?>
     <div class="field field-name-field-image field-type-image single-image">
         <img src="" />
