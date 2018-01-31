641572a99750a156d9641d49b6d136ff8a3a26f2
Charlie Llewellin <charlie@llewellin.net>
2018-01-31 14:47:52 -0600

    lampasas search

--- sites/all/themes/gavias_synery/template/field/field--field_image--article.tpl.php
+++ sites/all/themes/gavias_synery/template/field/field--field_image--article.tpl.php
@@ -46,9 +46,10 @@
  * @ingroup themeable
  */
 
+var_dump($items);
 if (count($items) == 1) { ?>
     <div class="field field-name-field-image field-type-image single-image">
-        <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>><?php print render($item); ?></div>
+        <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>><?php print render($items[0]); ?></div>
     </div>
     <?php } else { ?>
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
