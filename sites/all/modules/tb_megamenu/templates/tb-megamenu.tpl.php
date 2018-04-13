ce3fadc3ded37d1d0e94bdb0d33e2ad9a777e7b4
Charlie Llewellin <charlie@llewellin.net>
2018-04-13 15:51:04 -0500

    remove installed font-awesome from tb_megamenu module

--- sites/all/modules/tb_megamenu/templates/tb-megamenu.tpl.php
+++ sites/all/modules/tb_megamenu/templates/tb-megamenu.tpl.php
@@ -1,7 +1,7 @@
 <div <?php print $attributes;?> class="<?php print $classes;?>">
   <?php if($section == 'frontend') :?>
     <button data-target=".nav-collapse" data-toggle="collapse" class="btn btn-navbar tb-megamenu-button" type="button">
-      <i class="fas fa-reorder"></i>
+      <i class="fas fa-bars fa-2x" style="color:#000;"></i>
     </button>
     <div class="nav-collapse <?php print $block_config['always-show-submenu'] ? ' always-show' : '';?>">
   <?php endif;?>
