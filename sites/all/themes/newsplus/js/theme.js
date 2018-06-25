c220dc4b6444fcff3d337eaddf939bc6757f97f1
Charlie Llewellin <charlie@llewellin.net>
2018-06-25 14:41:13 -0700

    mobile menu

--- sites/all/themes/newsplus/js/theme.js
+++ sites/all/themes/newsplus/js/theme.js
@@ -1,19 +1,20 @@
 /* extra theme js */
 
-(function($) {
-  'use strict';
-  Drupal.behaviors.FontFixer = {
-    attach: function(context, settings){
-      $("#superfish-1").find("a.menuparent").each(function(i, obj) {
-        $(this).append('&nbsp;<i class="fas fa-caret-down" aria-hidden="true"></i>');
-      });
-      $(".block-search .form-actions, .block-search .trigger").append('<i' +
-          ' class="fas fa-search"' +
-          ' aria-hidden="true"></i>');
-      $(".more-link a").append('<i class="fas fa-chevron-right" aria-hidden="true"></i>');
-      $(".jcarousel-prev-horizontal").append('<i class="fas fa-arrow-left" aria-hidden="true"></i>');
-      $(".jcarousel-next-horizontal").append('<i class="fas fa-arrow-right" aria-hidden="true"></i>');
-    }
-  };
+(function ($) {
+    'use strict';
+    Drupal.behaviors.FontFixer = {
+        attach: function (context, settings) {
+            $("#superfish-1").find("a.menuparent").each(function (i, obj) {
+                $(this).append('&nbsp;<i class="fas fa-caret-down" aria-hidden="true"></i>');
+            });
+            $(".block-search .form-actions, .block-search .trigger").append('<i' +
+                ' class="fas fa-search"' +
+                ' aria-hidden="true"></i>');
+            $(".more-link a").append('<i class="fas fa-chevron-right" aria-hidden="true"></i>');
+            $(".jcarousel-prev-horizontal").append('<i class="fas fa-arrow-left" aria-hidden="true"></i>');
+            $(".jcarousel-next-horizontal").append('<i class="fas fa-arrow-right" aria-hidden="true"></i>');
+        }
+    };
+
 })(jQuery);
 
