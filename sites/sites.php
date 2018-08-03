2cd6d4a5463048c7c2e39e7eb4cedb3327c981bd
Charlie Llewellin <charlie@llewellin.net>
2018-08-03 19:18:31 +0100

    msgr.com

--- sites/sites.php
+++ sites/sites.php
@@ -88,6 +88,9 @@ $sites['www.themonitor.net'] = 'malakoffnews.etypegoogle10.com';
 /* lakeoconeenews.etypegoogle10.com */
 $sites['www.lakeoconeenews.us'] = 'lakeoconeenews.etypegoogle10.com';
 
+/* msgr.etypegoogle10.com */
+$sites['www.msgr.com'] = 'msgr.etypegoogle10.com';
+
 /* brownfieldonline.etypegoogle10.com */
 $sites['www.brownfieldonline.com'] = 'brownfieldonline.etypegoogle10.com';
 
