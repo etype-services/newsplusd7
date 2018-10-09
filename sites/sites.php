c12fb135359521b4080bd8ebba17a058d34994b4
Charlie Llewellin <charlie@llewellin.net>
2018-10-08 20:24:39 -0500

    vanzandtnews

--- sites/sites.php
+++ sites/sites.php
@@ -124,3 +124,6 @@ $sites['www.gothenburgtimes.com'] = 'gothenburgtimes.etypegoogle10.com';
 
 /* guthriecountynewspapers.etypegoogle10.com */
 $sites['www.guthriecountynewspapers.com'] = 'guthriecountynewspapers.etypegoogle10.com';
+
+/* vanzandtnews.etypegoogle10.com */
+$sites['www.vanzandtnews.com'] = 'vanzandtnews.etypegoogle10.com';
