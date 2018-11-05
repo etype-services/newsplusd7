112cf08b024783086b16032106be8f25168cda18
Charlie Llewellin <charlie@llewellin.net>
2018-11-05 11:15:31 -0600

    style fixes and flexslider

--- sites/all/themes/newsplus/templates/html.tpl.php
+++ sites/all/themes/newsplus/templates/html.tpl.php
@@ -9,7 +9,6 @@
     <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
  <![endif]-->
 <?php print $scripts; ?>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <div id="skip-link">
