8a8e0a0ec924108f9ffe4d83f52df36613650c04
Charlie Llewellin <charlie@llewellin.net>
2019-02-06 18:23:12 -0500

    kingfisher

--- sites/all/themes/newsplus/js/theme.js
+++ sites/all/themes/newsplus/js/theme.js
@@ -5,18 +5,48 @@
     Drupal.behaviors.FontFixer = {
         attach: function (context, settings) {
             $("#superfish-1").find("a.menuparent").each(function (i, obj) {
-                $(this).append('&nbsp;<i class="fas fa-caret-down" aria-hidden="true"></i>');
+                $(this).append('&nbsp;<i class="fas fa-caret-down"></i>');
             });
             $(".block-search .form-actions, .block-search .trigger").append('<i' +
                 ' class="fas fa-search"' +
                 ' aria-hidden="true"></i>');
-            $(".more-link a").append('<i class="fas fa-chevron-right" aria-hidden="true"></i>');
-            $(".jcarousel-prev-horizontal").append('<i class="fas fa-arrow-left" aria-hidden="true"></i>');
-            $(".jcarousel-next-horizontal").append('<i class="fas fa-arrow-right" aria-hidden="true"></i>');
+            $(".more-link a").append('<i class="fas fa-chevron-right"></i>');
+            $(".jcarousel-prev-horizontal").append('<i class="fas fa-arrow-left"></i>');
+            $(".jcarousel-next-horizontal").append('<i class="fas fa-arrow-right"></i>');
 
             /* fix search */
             $("#search-block-form .form-actions").click(function(){
                 $("#search-block-form").submit();
+            });
+        }
+    };
+
+    Drupal.behaviors.Internlabanner = {
+        attach: function (context, settings) {
+            $("#internal-banner-slider").fadeIn("slow");
+            $("#internal-slider-carousel").fadeIn("slow");
+
+            // The slider being synced must be initialized first
+            $("#internal-slider-carousel").flexslider({
+                animation: "slide",
+                controlNav: true,
+                animationLoop: false,
+                slideshow: false,
+                itemWidth: 166,
+                itemMargin: 4.8,
+                directionNav: false,
+                asNavFor: "#internal-banner-slider"
+            });
+
+            $("#internal-banner-slider").flexslider({
+                useCSS: true,
+                animation: "'.$internal_banner_effect.'",
+                controlNav: false,
+                keyboard: true,
+                directionNav: true,
+                animationLoop: false,
+                slideshow: false,
+                sync: "#internal-slider-carousel"
             });
         }
     };
