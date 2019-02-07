b84734d8e5de6744ac4308a4e90407324eb6e851
Charlie Llewellin <charlie@llewellin.net>
2019-02-06 19:21:02 -0500

    kingfisher

--- sites/all/themes/newsplus/js/theme.js
+++ sites/all/themes/newsplus/js/theme.js
@@ -7,8 +7,7 @@
             $("#superfish-1").find("a.menuparent").each(function (i, obj) {
                 $(this).append('&nbsp;<i class="fas fa-caret-down"></i>');
             });
-            $(".block-search .form-actions, .block-search .trigger").append('<i' +
-                ' class="fas fa-search"></i>');
+            $(".block-search .form-actions, .block-search .trigger").append('<i class="fas fa-search"></i>');
             $(".more-link a").append('<i class="fas fa-chevron-right"></i>');
             $(".jcarousel-prev-horizontal").append('<i class="fas fa-arrow-left"></i>');
             $(".jcarousel-next-horizontal").append('<i class="fas fa-arrow-right"></i>');
