dee283496b409ae9fdfc4cd7daf878aa31d84424
Charlie Llewellin <charlie@llewellin.net>
2019-02-06 19:20:35 -0500

    kingfisher

--- sites/all/themes/newsplus/js/theme.js
+++ sites/all/themes/newsplus/js/theme.js
@@ -8,8 +8,7 @@
                 $(this).append('&nbsp;<i class="fas fa-caret-down"></i>');
             });
             $(".block-search .form-actions, .block-search .trigger").append('<i' +
-                ' class="fas fa-search"' +
-                ' aria-hidden="true"></i>');
+                ' class="fas fa-search"></i>');
             $(".more-link a").append('<i class="fas fa-chevron-right"></i>');
             $(".jcarousel-prev-horizontal").append('<i class="fas fa-arrow-left"></i>');
             $(".jcarousel-next-horizontal").append('<i class="fas fa-arrow-right"></i>');
