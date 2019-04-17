343934f58a15b2de8851bb2858a65cc56713b19e
Charlie Llewellin <charlie@llewellin.net>
2019-04-17 14:56:17 -0500

    add dragscope theme

--- sites/dragscope.etypegoogle10.com/themes/dragscope/template.php
+++ sites/dragscope.etypegoogle10.com/themes/dragscope/template.php
@@ -4,7 +4,7 @@
 * @param $variables
 * Preprocess variables for page template.
 */
-function newsplus_preprocess_page(&$variables) {
+function dragscope_preprocess_page(&$variables) {
   $variables['header_top_inside_left_grid_class'] = 'col-md-4';
   $variables['header_top_inside_right_grid_class'] = 'col-md-8';
 }
