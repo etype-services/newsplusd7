b93091cc40e30d7be03f6b613df67c627ad1848a
Charlie Llewellin <charlie@llewellin.net>
2020-02-25 14:53:39 -0600

    external links

--- sites/all/themes/newsplus/js/theme.js
+++ sites/all/themes/newsplus/js/theme.js
@@ -1,31 +1,34 @@
 /* extra theme js */
 
 (function ($) {
-    'use strict';
-    Drupal.behaviors.ThemeFixer = {
-        attach: function (context, settings) {
+  'use strict';
+  Drupal.behaviors.ThemeFixer = {
+    attach: function (context, settings) {
 
-            /* Add fontawesome icons */
-            $("#superfish-1").find("a.menuparent").each(function (i, obj) {
-                $(this).append('&nbsp;<i class="fas fa-caret-down"></i>');
-            });
-            $(".block-search .form-actions, .block-search .trigger").append('<i class="fas fa-search"></i>');
-            $(".more-link a").append('<i class="fas fa-chevron-right"></i>');
-            $(".jcarousel-prev-horizontal").append('<i class="fas fa-arrow-left"></i>');
-            $(".jcarousel-next-horizontal").append('<i class="fas fa-arrow-right"></i>');
+      /* Add fontawesome icons */
+      $("#superfish-1").find("a.menuparent").each(function (i, obj) {
+        $(this).append('&nbsp;<i class="fas fa-caret-down"></i>');
+      });
+      $(".block-search .form-actions, .block-search .trigger").append('<i class="fas fa-search"></i>');
+      $(".more-link a").append('<i class="fas fa-chevron-right"></i>');
+      $(".jcarousel-prev-horizontal").append('<i class="fas fa-arrow-left"></i>');
+      $(".jcarousel-next-horizontal").append('<i class="fas fa-arrow-right"></i>');
 
-            /* Fix search */
-            $("#search-block-form .form-actions").click(function(){
-                $("#search-block-form").submit();
-            });
+      /* Fix search */
+      $("#search-block-form .form-actions").click(function () {
+        $("#search-block-form").submit();
+      });
 
-            /* Internal banner captions */
-            $(".views-field-field-mt-banner-image .field-content img").each(function(idx,img) {
-                var caption = img.alt || img.title;
-                $(this).parent().append("<div class=\"banner-caption\"><div class=\"caption-title\">" + caption + "</div></div>");
-            });
-        }
-    };
+      /* Internal banner captions */
+      $(".views-field-field-mt-banner-image .field-content img").each(function (idx, img) {
+        var caption = img.alt || img.title;
+        $(this).parent().append("<div class=\"banner-caption\"><div class=\"caption-title\">" + caption + "</div></div>");
+      });
+
+      /* External links */
+      $("a.ext").prop("target", "_blank");
+    }
+  };
 
 })(jQuery);
 
