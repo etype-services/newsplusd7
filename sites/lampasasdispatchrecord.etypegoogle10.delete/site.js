d55ca9fcc9721572d16918e56720c23e49c62d8d
etypeweb <etypeweb@newsplus.c.modern-photon-112115.internal>
2021-04-21 15:35:34 -0500

    Drupal 7.80

--- sites/lampasasdispatchrecord.etypegoogle10.delete/site.js
+++
@@ -1,7 +0,0 @@
-(function ($) {
-    Drupal.behaviors.lampasas = {
-        attach: function () {
-            $("#search-block-form .form-submit").after('<i class="fa fa-search"></i>');
-        }
-    };
-})(jQuery);
